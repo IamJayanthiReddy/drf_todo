c9ea885056fe49a6f3d3319eced28c0d82cdb736
t-jreddivari <t-jreddivari@microsoft.com>
2021-05-17 10:06:08 +0530

    Todo app in django_rest_framework

---
+++ todos/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Todo
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ("user", "name", "done", "date_created")
+    list_filter = ("done", "date_created")
+
+
+admin.site.register(Todo, TodoAdmin)
